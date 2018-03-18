b02fb5f21950952f8ad1bedb7745b6a25e3a64da
abhishek18620 <abhishek18620@gmail.com>
2018-03-18 16:57:07 +0530

    Regular Backup

---
+++ codeforces/kickstart/2018/A_P-1.cpp
@@ -0,0 +1,134 @@
+/******************************************
+*  Author : abhishek18620
+*  Created On : 2018-03-18
+*  File : A_P-1
+*******************************************/
+// It's my template. Don't you dare to selct and copy it ;)
+#pragma comment (linker, "/ STACK: 100000000")
+#include <bits/stdc++.h>
+using namespace std;
+#define M 20010
+#define INF 999999
+#define fio ios::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+#define f(i, j, k) for (int i = j; i < k; i++)
+#define fr(i, j, k) for (int i = j; i >= k; i--)
+#define gc() getchar_unlocked()
+#define test int t; scan(t); while(t--)
+#define mp(i,j) make_pair(i,j)
+#define F first
+#define S second
+#define pb push_back
+#define error(args...) { vector<string> _v = split(#args, ','); err(_v.begin(), args); }
+#define scan(d) scanf("%d", &d)
+#define scan2(a, b) scanf("%d %d", &a, &b)
+#define scan3(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define scan4(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define scanl(d) scanf("%lld", &d)
+#define scanl2(a, b) scanf("%lld %lld", &a, &b)
+#define scanl3(a, b, c) scanf("%lld %lld %lld", &a, &b, &c)
+#define scanl4(a, b, c, d) scanf("%lld %lld %lld %lld", &a, &b, &c, &d)
+#define print(t) printf("%d\n",t)
+#define printl(t) printf("%lld\n",t)
+#define trace2(x, y)          cerr <<#x<<": "<<x<<" | "<<#y<<": "<<y<< endl;
+#define trace3(x, y, z)       cerr <<#x<<": "<<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl;
+#define trace4(a, b, c, d)    cerr <<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl;
+#define trace5(a, b, c, d, e) cerr <<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<<": "<<e<<endl;
+#define eb emplace_back
+#define PI 3.1415926535897932384626433832795
+#define viter(it,s) for (auto it: s)
+#define foreach(v, c) for(__typeof( (c).begin()) v = (c).begin();  v != (c).end(); ++v)
+#define foreachr(v, c) for(__typeof( (c).rbegin()) v = (c).rbegin();  v != (c).rend(); ++v)
+#define ALL(v) (v).begin(), (v).end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef set<int>::iterator sit;
+typedef map<int,int>::iterator mit;
+typedef vector<int>::iterator vit;
+typedef vector<pii> vii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+#ifdef LOCAL_DEFINE
+    #include<assert.h>
+#else
+    #define debug(args...)                  // Just strip off all debug tokens
+#endif
+
+const int MOD = 1000000007;
+#define MODSET(d) if ((d) >= MOD) d %= MOD;
+#define MODNEGSET(d) if ((d) < 0) d = ((d % MOD) + MOD) % MOD;
+#define MODADDSET(d) if ((d) >= MOD) d -= MOD;
+#define MODADDWHILESET(d) while ((d) >= MOD) d -= MOD;
+const int SQRTN = 300;
+const int LOGN = 16;
+const int INT_INFINITY = 1001001001;
+const ll LONG_INFINITY = 2001001001001001001ll;
+const double EPS = 1e-6;
+
+int scanstr(char *str)
+{
+    register char c = getchar();
+    register int len = 0;
+    if(!(~c)) return -1;
+    while(c < 33 && ~c) c = getchar();
+    while(c != 10 && c != 32 && ~c){
+        str[len] = c;
+        c = getchar();
+        len++;
+    }
+    str[len] = '\0';
+    return 1;
+}
+
+int power(int x, unsigned int y)
+{
+    int res = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = res*x;
+
+        y = y>>1; x = x*x;
+    }
+    return res;
+}
+//-------------------------------------------------------END OF TEMPLATE---------------------------------------------------------------------------
+
+const int K=50010;
+double dp[K];
+ll a[M],sum[M];
+
+int main()
+{
+    #ifdef LOCAL_DEFINE
+        clock_t tStart = clock();
+        freopen("input.txt","rt",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    int t; scan(t);
+    f(tt,1,t+1)
+    {
+        int n,k;
+        scan2(n,k);
+        f(i,1,n+1) scanl(a[i]);
+        sort(a+1,a+n+1);
+        sum[0]=0;
+        f(i,1,n+1) sum[i]=sum[i-1]+a[i];
+        dp[0]=0;
+        f(i,1,k+2)
+        {
+            int till=upper_bound(a+1,a+n+1,floor(dp[i-1]))-a;
+            //trace3(i,till,dp[i-1]);
+            dp[i]=((dp[i-1]*(till-1)) + ((sum[n]-sum[till-1])*1.0))/n;
+        }
+        double ans=dp[k+1];
+        printf("Case #%d: %0.9lf\n",tt,ans);
+    }
+    //assert((1.0*(clock()-tStart)/CLOCKS_PER_SEC)<1.0);     // time limit to avoid infinite loops
+    #ifdef LOCAL_DEFINE
+        cerr<<"Time elapsed: "<<1.0*(clock()-tStart)/CLOCKS_PER_SEC<<" s.\n";
+        cin>>t;
+    #endif
+    return 0;
+}
