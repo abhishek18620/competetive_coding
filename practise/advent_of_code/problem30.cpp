635cf1ffda3b5514f27806c04cc9ad556c2f9dca
abhishek18620 <abhishek18620@gmail.com>
2020-12-15 16:42:38 +0530

    Create problem30.cpp

---
+++ practise/advent_of_code/problem30.cpp
@@ -0,0 +1,64 @@
+// https://adventofcode.com/2020/day/1
+#include <bitset>
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+
+auto get_occ = [](std::map<int, std::pair<int, int>>& numbers_map, int ele) {
+    if (numbers_map.find(ele) == numbers_map.end()) {
+        return 0;
+    }
+    if (numbers_map.at(ele).second == -1) {
+        return 1;
+    }
+    return 2;
+};
+
+int Solve(const std::vector<int>& starting_nums) {
+    std::map<int, std::pair<int, int>> numbers_map;
+    std::vector<int> turn;
+    for (int i = 0; i < starting_nums.size(); ++i) {
+        numbers_map[starting_nums[i]] = {i+1, -1};
+    }
+    int curr_turn = starting_nums.size();
+    int last_num = starting_nums[starting_nums.size() - 1];
+
+    // Simulate game
+    while (curr_turn++ != 30000000) {
+        int occ = get_occ(numbers_map, last_num);
+        if (occ == 1) {
+            last_num = 0;
+        } else {
+            // occ > 1
+            last_num = numbers_map.at(last_num).second - numbers_map.at(last_num).first;
+        }
+#ifdef DEBUG
+        if (curr_turn % 1000000 == 0) {
+            printf("curr_turn = %d last_num = %d\n", curr_turn, last_num);
+        }
+#endif
+
+        occ = get_occ(numbers_map, last_num);
+        if (occ == 0) {
+            numbers_map[last_num] = {curr_turn , -1};
+        } else if (occ == 1) {
+            numbers_map.at(last_num).second = curr_turn;
+        } else {
+            std::swap(numbers_map.at(last_num).first, numbers_map.at(last_num).second);
+            numbers_map.at(last_num).second = curr_turn;
+        }
+        // printf("last f = %d s = %d\n", numbers_map.at(last_num).first, numbers_map.at(last_num).second);
+    }
+
+    return last_num;
+}
+
+int main() {
+#ifdef DEBUG
+    freopen("input.txt", "rt", stdin);
+#endif
+    Solve({15, 5, 1, 4, 7, 0});
+    return 0;
+}
